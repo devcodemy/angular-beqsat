a849f7c298525d012155569afde6b515d7ee61fe
devcodemy <60103139+devcodemy@users.noreply.github.com>
2022-11-20 01:52:02 +0200

    4 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,10 +8,15 @@ import { products } from '../products';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  products = products;
+  // products = products;
+  products = [...products];
 
   share() {
     window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be nitified when the product goes on sale');
   }
 }
 
